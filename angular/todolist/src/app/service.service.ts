9d460b60dbfb1840d16e322006fb9b2031301cc5
Yasminabdallah <yasminabdalllahgaberm@gmail.com>
2018-07-05 14:51:29 +0200

    delete header

--- angular/todolist/src/app/service.service.ts
+++ angular/todolist/src/app/service.service.ts
@@ -5,7 +5,7 @@ import { Observable } from 'rxjs/Observable';
 
 
 const path: string = "http://127.0.0.1:8000/api"
-const headers = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded' });
+
 
 @Injectable({
   providedIn: 'root'
@@ -27,7 +27,7 @@ export class ServiceService  {
 
   deleteData(url:string):Observable<any>{
 
-    return this.http.delete(path+url,{ headers });
+    return this.http.delete(path+url);
    
   }
 }
